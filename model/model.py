ac794182057f96e2b679bea113214220fa269268
cxy-thinkbook <xuanyuchen@seu.edu.cn>
2026-03-12 11:52:25 +0800

    model & causalLM

--- model/model.py
+++ model/model.py
@@ -76,8 +76,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from transformers.activations import ACT2FN
-
-from typing import Optional, Tuple
+from transformers import PretrainedModel, GenerationMixin
+from transformers.modeling_outputs import CausalLMOutputWithPast
+from typing import Optional, Tuple, Union
 
 # RMSNorm的实现
 class RMSNorm(nn.Module):
@@ -303,3 +304,108 @@ class MokioMindBlock(nn.Module):
         hidden_states = residual + hidden_states
         hidden_states = hidden_states + self.mlp(self.post_attention_layernorm(hidden_states))
         return hidden_states, present_key_value
+
+class MokioMindModel(nn.Module):
+    def __init__(self, config: MokioMindConfig):
+        super().__init__()
+        self.vocab_size, self.num_hidden_layers = (
+            config.vocab_size, config.num_hidden_layers
+        )
+
+        self.embed_tokens = nn.Embedding(self.vocab_size, config.hidden_size)
+
+        self.dropout = nn.Dropout(config.dropout)
+
+        self.layers = nn.ModuleList([MokioMindBlock(i, config) for i in range(self.num_hidden_layers)])
+
+        self.norm = RMSNorm(config.hidden_size, eps=config.rms_norm_eps)
+
+        # RoPE 预计算
+        freqs_cos, freqs_sin = precompute_freqs_cls(
+            dim = config.hidden_size // config.num_attention_heads,
+            end = config.max_position_embeddings,
+            rope_base = config.rope_theta,
+            rope_scaling = config.rope_scaling
+        )
+        self.register_buffer("freqs_cos", freqs_cos, persistent=False)
+        self.register_buffer("freqs_sin", freqs_sin, persistent=False)
+
+    def forward(self, 
+    input_ids: torch.Tensor, 
+    attention_mask: Optional[torch.Tensor] = None,
+    past_key_values: Optional[Tuple[Tuple[torch.Tensor]]] = None,
+    use_cache: bool = False,
+    **kwargs,
+    ):
+        batch_size, seq_length = input_ids.shape
+
+        if hasattr(past_key_values, "layers"):
+            past_key_values = None
+
+        past_key_values = past_key_values or [None] * len(self.layers)
+
+        start_pos = (
+            past_key_values[0][0].shape[1] if past_key_values[0] is not None else 0
+        )
+
+        hidden_states = self.dropout(self.embed_tokens(input_ids))
+
+        position_embeddings = (
+            self.freqs_cos[start_pos: start_pos + seq_length],
+            self.freqs_sin[start_pos: start_pos + seq_length],
+        )
+
+        presents = ()
+
+        for layer_idx, (layer, past_key_value) in enumerate(zip(self.layers, past_key_values)):
+            hidden_states, present_key_value = layer(
+                hidden_states,
+                position_embeddings,
+                past_key_value = past_key_value,
+                use_cache = use_cache,
+                attention_mask = attention_mask,
+            )
+            presents = presents + (present_key_value,)
+
+        hidden_states = self.norm(hidden_states)
+        return hidden_states, presents
+
+class MokioMindForCausalLM(PretrainedModel, GenerationMixin):
+    config_class = MokioMindConfig
+
+    def __init__(self, config: MokioMindConfig):
+        self.config = config
+        
+        super().__init__(config)
+
+        self.model = MokioMindModel(config)
+
+        self.lm_head = nn.Linear(config.hidden_size, config.vocab_size, bias=False)
+
+        self.model.embed_tokens.weight = self.lm_head.weight
+
+        self.OUT = CausalLMOutputWithPast()
+
+    def forward(self, input_ids: torch.Tensor, 
+    attention_mask: Optional[torch.Tensor] = None, 
+    past_key_values: Optional[Tuple[Tuple[torch.Tensor]]] = None, 
+    use_cache: bool = False, 
+    logits_to_keep: Union[int, torch.Tensor] = 0,
+    **args):
+        hidden_states, past_key_values = self.model(
+            input_ids = input_ids,
+            attention_mask = attention_mask,
+            past_key_values = past_key_values,
+            use_cache = use_cache,
+            **args,
+        )
+
+        slice_indices = (slice(-logits_to_keep) if isinstance(logits_to_keep, int) else logits_to_keep)
+        logits = self.lm_head(hidden_states[:, slice_indices, :])
+
+        self.OUT.__setitem__("last_hidden_state", hidden_states)
+        self.OUT.__setitem__("past_key_values", past_key_values)
+        self.OUT.__setitem__("logits", logits)
+        
+        return self.OUT
+
