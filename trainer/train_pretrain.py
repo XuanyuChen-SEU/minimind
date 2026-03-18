9a6c055c85cea75e2252dc1efaf889e71c3edd7f
cxy <xuanyuchen@seu.edu.cn>
2026-03-19 00:04:35 +0800

    加点没用的

--- trainer/train_pretrain.py
+++ trainer/train_pretrain.py
@@ -76,7 +76,9 @@ def train_epoch(
         for param_group in optimizer.param_groups:
             param_group["lr"] = lr
 
-        with autocast_ctx:
+        with autocast_ctx: 
+            # 在上下文范围内自动将模型的计算和数据在 FP32（单精度）和 FP16（半精度）之间切换
+            # 以在保证精度的前提下提升训练 / 推理速度、降低显存占用。
             res = model(input_ids, attention_mask=attention_mask, labels=labels)
             loss = res.loss  # 预训练不加 MoE aux_loss
             loss = loss / args.accumulation_steps
@@ -94,7 +96,7 @@ def train_epoch(
 
         if step % args.log_interval == 0 or step == iters:
             spend_time = time.time() - start_time
-            current_loss = loss.item() * args.accumulation_steps
+            current_loss = loss.item() * args.accumulation_steps  # 恢复真实损失值，把梯度累积的缩放乘回来
             current_lr = optimizer.param_groups[-1]["lr"]
             eta_min = spend_time / (step + 1) * iters // 60 - spend_time // 60
 
